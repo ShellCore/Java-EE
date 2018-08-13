9566866c1a3f08da30bd2b23d7356d403443e34e
Cesar Morales Garduño <shell.one.core@gmail.com>
2018-08-13 16:34:03 -0500

    Realización del ejercicio 26: Laboratorio: Cliente SGA Web Service con JAX-WS con Java EE

--- ClienteSgaWS/src/main/java/clientesga/ws/ObjectFactory.java
+++ ClienteSgaWS/src/main/java/clientesga/ws/ObjectFactory.java
@@ -25,13 +25,37 @@ import javax.xml.namespace.QName;
 public class ObjectFactory {
 
     private final static QName _ListaPersonasResponse_QNAME = new QName("http://servicio.sga.gm.com.mx/", "listaPersonasResponse");
+    private final static QName _ModificarPersona_QNAME = new QName("http://servicio.sga.gm.com.mx/", "modificarPersona");
+    private final static QName _EncontrarPersonaPorIdResponse_QNAME = new QName("http://servicio.sga.gm.com.mx/", "encontrarPersonaPorIdResponse");
     private final static QName _ListaPersonas_QNAME = new QName("http://servicio.sga.gm.com.mx/", "listaPersonas");
+    private final static QName _RegistrarPersona_QNAME = new QName("http://servicio.sga.gm.com.mx/", "registrarPersona");
+    private final static QName _EliminarPersona_QNAME = new QName("http://servicio.sga.gm.com.mx/", "eliminarPersona");
+    private final static QName _EliminarPersonaResponse_QNAME = new QName("http://servicio.sga.gm.com.mx/", "eliminarPersonaResponse");
+    private final static QName _EncontrarPersonaPorId_QNAME = new QName("http://servicio.sga.gm.com.mx/", "encontrarPersonaPorId");
+    private final static QName _ModificarPersonaResponse_QNAME = new QName("http://servicio.sga.gm.com.mx/", "modificarPersonaResponse");
+    private final static QName _RegistrarPersonaResponse_QNAME = new QName("http://servicio.sga.gm.com.mx/", "registrarPersonaResponse");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: clientesga.ws
      * 
      */
     public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link EliminarPersona }
+     * 
+     */
+    public EliminarPersona createEliminarPersona() {
+        return new EliminarPersona();
+    }
+
+    /**
+     * Create an instance of {@link RegistrarPersona }
+     * 
+     */
+    public RegistrarPersona createRegistrarPersona() {
+        return new RegistrarPersona();
     }
 
     /**
@@ -43,11 +67,59 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link EncontrarPersonaPorIdResponse }
+     * 
+     */
+    public EncontrarPersonaPorIdResponse createEncontrarPersonaPorIdResponse() {
+        return new EncontrarPersonaPorIdResponse();
+    }
+
+    /**
      * Create an instance of {@link ListaPersonasResponse }
      * 
      */
     public ListaPersonasResponse createListaPersonasResponse() {
         return new ListaPersonasResponse();
+    }
+
+    /**
+     * Create an instance of {@link ModificarPersona }
+     * 
+     */
+    public ModificarPersona createModificarPersona() {
+        return new ModificarPersona();
+    }
+
+    /**
+     * Create an instance of {@link EncontrarPersonaPorId }
+     * 
+     */
+    public EncontrarPersonaPorId createEncontrarPersonaPorId() {
+        return new EncontrarPersonaPorId();
+    }
+
+    /**
+     * Create an instance of {@link ModificarPersonaResponse }
+     * 
+     */
+    public ModificarPersonaResponse createModificarPersonaResponse() {
+        return new ModificarPersonaResponse();
+    }
+
+    /**
+     * Create an instance of {@link RegistrarPersonaResponse }
+     * 
+     */
+    public RegistrarPersonaResponse createRegistrarPersonaResponse() {
+        return new RegistrarPersonaResponse();
+    }
+
+    /**
+     * Create an instance of {@link EliminarPersonaResponse }
+     * 
+     */
+    public EliminarPersonaResponse createEliminarPersonaResponse() {
+        return new EliminarPersonaResponse();
     }
 
     /**
@@ -68,12 +140,84 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ModificarPersona }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "modificarPersona")
+    public JAXBElement<ModificarPersona> createModificarPersona(ModificarPersona value) {
+        return new JAXBElement<ModificarPersona>(_ModificarPersona_QNAME, ModificarPersona.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EncontrarPersonaPorIdResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "encontrarPersonaPorIdResponse")
+    public JAXBElement<EncontrarPersonaPorIdResponse> createEncontrarPersonaPorIdResponse(EncontrarPersonaPorIdResponse value) {
+        return new JAXBElement<EncontrarPersonaPorIdResponse>(_EncontrarPersonaPorIdResponse_QNAME, EncontrarPersonaPorIdResponse.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link ListaPersonas }{@code >}}
      * 
      */
     @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "listaPersonas")
     public JAXBElement<ListaPersonas> createListaPersonas(ListaPersonas value) {
         return new JAXBElement<ListaPersonas>(_ListaPersonas_QNAME, ListaPersonas.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RegistrarPersona }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "registrarPersona")
+    public JAXBElement<RegistrarPersona> createRegistrarPersona(RegistrarPersona value) {
+        return new JAXBElement<RegistrarPersona>(_RegistrarPersona_QNAME, RegistrarPersona.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EliminarPersona }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "eliminarPersona")
+    public JAXBElement<EliminarPersona> createEliminarPersona(EliminarPersona value) {
+        return new JAXBElement<EliminarPersona>(_EliminarPersona_QNAME, EliminarPersona.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EliminarPersonaResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "eliminarPersonaResponse")
+    public JAXBElement<EliminarPersonaResponse> createEliminarPersonaResponse(EliminarPersonaResponse value) {
+        return new JAXBElement<EliminarPersonaResponse>(_EliminarPersonaResponse_QNAME, EliminarPersonaResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link EncontrarPersonaPorId }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "encontrarPersonaPorId")
+    public JAXBElement<EncontrarPersonaPorId> createEncontrarPersonaPorId(EncontrarPersonaPorId value) {
+        return new JAXBElement<EncontrarPersonaPorId>(_EncontrarPersonaPorId_QNAME, EncontrarPersonaPorId.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ModificarPersonaResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "modificarPersonaResponse")
+    public JAXBElement<ModificarPersonaResponse> createModificarPersonaResponse(ModificarPersonaResponse value) {
+        return new JAXBElement<ModificarPersonaResponse>(_ModificarPersonaResponse_QNAME, ModificarPersonaResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link RegistrarPersonaResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://servicio.sga.gm.com.mx/", name = "registrarPersonaResponse")
+    public JAXBElement<RegistrarPersonaResponse> createRegistrarPersonaResponse(RegistrarPersonaResponse value) {
+        return new JAXBElement<RegistrarPersonaResponse>(_RegistrarPersonaResponse_QNAME, RegistrarPersonaResponse.class, null, value);
     }
 
 }
--- ClienteSgaWS/src/main/java/testclientews/TestPersonaServiceWS.java
+++ ClienteSgaWS/src/main/java/testclientews/TestPersonaServiceWS.java
@@ -6,14 +6,78 @@ import clientesga.ws.PersonaServiceWS;
 import java.util.List;
 
 public class TestPersonaServiceWS {
+
+    static PersonaServiceWS service;
+    static Persona persona1;
     
     public static void main(String[] args) {
-        PersonaServiceWS service = new PersonaServiceImplService().getPersonaServiceImplPort();
-        System.out.println("Ejecutando el servicio Listar personas WS");
+        service = new PersonaServiceImplService().getPersonaServiceImplPort();
+        
+        probarListarPersonas();
+//        probarAgregarPersona();
+        probarModificarPersona();
+        probarEliminarPersona();
+    }
+
+    private static void probarListarPersonas() {
+        System.out.println("\nEjecutando el servicio Listar personas WS");
         List<Persona> list = service.listaPersonas();
         list.forEach(p -> {
             System.out.println("Persona {" + p.getNombre() + " " + p.getPaterno() + "}");
         });
         System.out.println("Fin del servicio Listar personas WS");
+    }
+    
+    private static void probarAgregarPersona() {
+        System.out.println("\nEjecutando el servicio Registrar persona WS");
+        persona1 = new Persona();
+        persona1.setNombre("Prueba");
+        persona1.setPaterno("Prueba");
+        persona1.setEmail("Prueba@mail.com");
+        imprimirPersona();
+        service.registrarPersona(persona1);
+        imprimirPersona();
+        System.out.println("Fin del servicio Registrar personas WS");
+    }
+    
+    private static void probarModificarPersona() {
+        System.out.println("\nEjecutando el servicio Modificar persona WS");
+        Persona pSearch = new Persona();
+        pSearch.setId(6);
+        persona1 = service.encontrarPersonaPorId(pSearch);
+        System.out.print("Persona encontrada: ");
+        imprimirPersona();
+        persona1.setPaterno("PruebaModificada");
+        System.out.print("Modificando apellido: ");
+        imprimirPersona();
+        service.modificarPersona(persona1);
+        System.out.print("Persona modificada: ");
+        imprimirPersona();
+        
+        System.out.println("Fin del servicio Modificar persona WS");
+    }
+    
+    private static void probarEliminarPersona() {
+        System.out.println("\nEjecutando el servicio Modificar persona WS");
+        Persona pSearch = new Persona();
+        pSearch.setId(6);
+        persona1 = service.encontrarPersonaPorId(pSearch);
+        System.out.print("Persona encontrada: ");
+        imprimirPersona();
+        System.out.print("Eliminando persona: ");
+        imprimirPersona();
+        service.eliminarPersona(persona1);
+        System.out.println("Persona eliminada!");
+        System.out.println("Fin del servicio Modificar persona WS");
+    }
+    
+    private static void imprimirPersona() {
+        String message = "Persona {"
+                + "Id: " + persona1.getId()
+                + ", Nombre: " + persona1.getNombre()
+                + ", Paterno: " + persona1.getPaterno()
+                + ", Email: " + persona1.getEmail()
+                + "}";
+        System.out.println(message);
     }
 }
